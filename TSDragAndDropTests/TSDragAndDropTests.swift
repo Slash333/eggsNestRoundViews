1c7a724f01c1df352d4ff010b51112ca5e44fb2a
Igor Ponomarenko <igorponomarenko@Igors-MacBook-Pro-2.local>
2015-06-12 13:52:03 +0300

    Initial Commit

---
+++ TSDragAndDropTests/TSDragAndDropTests.swift
@@ -0,0 +1,36 @@
+//
+//  TSDragAndDropTests.swift
+//  TSDragAndDropTests
+//
+//  Created by Igor Ponomarenko on 6/12/15.
+//  Copyright (c) 2015 Igor Ponomarenko. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class TSDragAndDropTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
