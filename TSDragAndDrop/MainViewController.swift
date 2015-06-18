c512160e2bcb48f3fca9070525c0f5217fe5c914
Igor Ponomarenko <igorponomarenko@Igors-MacBook-Pro-2.local>
2015-06-18 14:02:44 +0300

    check OB

--- TSDragAndDrop/MainViewController.swift
+++ TSDragAndDrop/MainViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class MainViewController: UIViewController, DNDDragSourceDelegate/*, DNDDropTargetDelegate*/ {
+class MainViewController: UIViewController, OBOvumSource, OBDropZone/*, DNDDragSourceDelegate, DNDDropTargetDelegate*/ {
 
     @IBOutlet var dragAndDropController: DNDDragAndDropController!
     
@@ -18,11 +18,18 @@ class MainViewController: UIViewController, DNDDragSourceDelegate/*, DNDDropTarg
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let dragRecognizer = DNDLongPressDragRecognizer()
-        dragRecognizer.minimumPressDuration = 0.1
-        eggTableViewController?.tableView.panGestureRecognizer.requireGestureRecognizerToFail(dragRecognizer)
+        //let dragRecognizer = DNDLongPressDragRecognizer()
+        //dragRecognizer.minimumPressDuration = 0.1
         
-        dragAndDropController.registerDragSource(eggTableViewController?.tableView, withDelegate: self, dragRecognizer: dragRecognizer)
+        //eggTableViewController?.tableView.panGestureRecognizer.requireGestureRecognizerToFail(dragRecognizer)
+        
+        //dragAndDropController.registerDragSource(eggTableViewController?.tableView, withDelegate: self, dragRecognizer: dragRecognizer)
+        //dragAndDropController.registerDropTarget(eggTableViewController?.tableView, withDelegate: self)
+        
+        var ddManager = OBDragDropManager.sharedManager()
+        
+        var gesture = ddManager.createLongPressDragDropGestureRecognizerWithSource(self)
+        eggTableViewController?.tableView.addGestureRecognizer(gesture)
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
@@ -35,6 +42,41 @@ class MainViewController: UIViewController, DNDDragSourceDelegate/*, DNDDropTarg
         }
     }
     
+    // MARK: OBOvumSource
+    
+    func createOvumFromView(sourceView: UIView!) -> OBOvum! {
+        var ovum = OBOvum()
+        ovum.dataObject = NSNumber(integer: sourceView.tag)
+        return ovum
+    }
+    
+    func createDragRepresentationOfSourceView(sourceView: UIView!, inWindow window: UIWindow!) -> UIView! {
+        var draggingView = NSBundle.mainBundle().loadNibNamed("GhostEgg", owner: nil, options: nil).first as! GhostEgg
+        view.addSubview(draggingView)
+        
+        draggingView.center = sourceView.center
+        return draggingView
+    }
+    
+    
+    
+    // MARK: OBDropZone
+    
+    func ovumDropped(ovum: OBOvum!, inView view: UIView!, atLocation location: CGPoint) {
+        NSLog("ovumDropped");
+    }
+    
+    func ovumEntered(ovum: OBOvum!, inView view: UIView!, atLocation location: CGPoint) -> OBDropAction {
+        NSLog("ovumEntered");
+        
+        return OBDropAction.Move
+    }
+    
+    func ovumExited(ovum: OBOvum!, inView view: UIView!, atLocation location: CGPoint) {
+        NSLog("ovumExited");
+    }
+    
+    /*
     // MARK: DNDDragSourceDelegate
     
     func draggingViewForDragOperation(operation: DNDDragOperation!) -> UIView! {
@@ -53,7 +95,31 @@ class MainViewController: UIViewController, DNDDragSourceDelegate/*, DNDDropTarg
         operation.removeDraggingViewAnimatedWithDuration(0.5, animations: { (draggingView: UIView!) -> Void in
             draggingView.alpha = 0
             draggingView.center = self.view.convertPoint(operation.dragSourceView.center, fromView: operation.dragSourceView)
-            
+            //operation.removeDraggingView()
         })
+        
+        
     }
+    
+    // MARK: DNDDropTargetDelegate
+    
+    func dragOperation(operation: DNDDragOperation!, didDropInDropTarget target: UIView!) {
+        var view = target;
+        var x = 1
+        x = 3
+        
+        operation.removeDraggingView()
+    }
+    
+    func dragOperation(operation: DNDDragOperation!, didEnterDropTarget target: UIView!) {
+        var view = target;
+        var x = 1
+        x = 3
+    }
+    
+    func dragOperation(operation: DNDDragOperation!, shouldPositionDragViewInDropTarget target: UIView!) -> Bool {
+        return true
+    }
+*/
+    
 }
--- TSDragAndDrop/TSDragAndDrop-Bridging-Header.h
+++ TSDragAndDrop/TSDragAndDrop-Bridging-Header.h
@@ -3,3 +3,4 @@
 //
 
 #import "DNDDragAndDrop.h"
+#import "OBDragDrop.h"
